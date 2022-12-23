9aa1b50e2d6b358c5b09bef4cabf7b8e52e87feb
Yamil Daza <devyamil1998@gmail.com>
2022-12-23 18:39:27 -0300

    finishedWork_Task4_1

--- src/projects/Task4/MyTask4_1/App.jsx
+++ src/projects/Task4/MyTask4_1/App.jsx
@@ -1,49 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Rey from './Rey';
+import './rey.css';
 
 const reyes=[
    {
       nombre:"Atanagildo",
       color:"darkolivegreen",
-      precio:178
+      precio:178,
+      img: 'https://www.html6.es/img/rey_atanagildo.png',
    },
    {
       nombre:"Ervigio",
       color:"crimson",
-      precio:169
+      precio:169,
+      img: 'https://www.html6.es/img/rey_ervigio.png',
    },
    {
       nombre:"Ataúlfo",
       color:"peru",
-      precio:81
+      precio:81,
+      img:'https://www.html6.es/img/rey_ata%C3%BAlfo.png',
    },
    {
       nombre:"Leogivildo",
       color:"darkmagenta",
-      precio:126
+      precio:126,
+      img: 'https://www.html6.es/img/rey_leogivildo.png',
    },
    {
       nombre:"Recesvinto",
       color:"royalblue",
-      precio:141
+      precio:141,
+      img: 'https://www.html6.es/img/rey_recesvinto.png',
    },
    {
       nombre:"Sisebuto",
       color:"teal",
-      precio:69
+      precio:69,
+      img: 'https://www.html6.es/img/rey_sisebuto.png',
    }
 ];
 
 const App = () => {
+   const [total, setTotal] = useState(0);
+
    return (
       <div>
          <h1>Reyes Godo</h1>
-         <Rey data={reyes[0]}/>
-         <Rey data={reyes[1]}/>
-         <Rey data={reyes[2]}/>
-         <Rey data={reyes[3]}/>
-         <Rey data={reyes[4]}/>
-         <Rey data={reyes[5]}/>
+            <h2>Total a pagar : ${total}</h2>
+         <div className='reyes'>
+            <Rey data={reyes[0]} set={setTotal}/>
+            <Rey data={reyes[1]} set={setTotal}/>
+            <Rey data={reyes[2]} set={setTotal}/>
+            <Rey data={reyes[3]} set={setTotal}/>
+            <Rey data={reyes[4]} set={setTotal}/>
+            <Rey data={reyes[5]} set={setTotal}/>
+         </div>
       </div>
    );
 }
--- src/projects/Task4/MyTask4_1/Rey.jsx
+++ src/projects/Task4/MyTask4_1/Rey.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import './rey.css';
 
-const Rey = ( props ) => {
-   const {nombre, color, precio} = props.data;
+const Rey = ( { data, set} ) => {
+   const { nombre, color, precio, img } = data;
+   
+   function comprarRey(precio){
+      set((e) => e + precio);
+      document.querySelector('.contenedor-rey').remove();
+   }
+   
+   
    return (
       <>
-         <div className='contenedor-rey'>
-            <h2>nombre: {nombre}</h2>
-            <img/>
+         <div className='contenedor-rey' style={{backgroundColor:color}}>
+            <h1>Rey Godo</h1>
+            <img src={img} alt={`Rey_${nombre}`}/>
+            <h3>Nombre: {nombre}</h3>
             <p>Precio: ${precio}</p>
+            <button onClick={() => comprarRey(precio)}>Comprar</button>
          </div>
       </>
    );
