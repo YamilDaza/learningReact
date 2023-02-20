1b5c969f4a84251400f9a6c3a0434d1d26b1888f
dev yamil <devyamil1998@gmail.com>
2023-02-20 20:07:04 -0300

    cssFramework

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 // import App from './App';
 import './styles/index.css';
 import MainTaks from './projects/MainTaks';
-import Practice from './Practice'
+import Practice from './Practice';
+import Interfaz from './Interfaz';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		{/* <App /> */}
 		{/* <MainTaks></MainTaks> */}
-		<Practice>My component of practice</Practice>
+		{/* <Practice>My component of practice</Practice> */}
+		<Interfaz></Interfaz>
 	</React.StrictMode>
 )
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/Main.jsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
